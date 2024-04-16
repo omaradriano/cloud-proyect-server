9fce97da79b2d3d0ec832ed6bd650175c89602ed
Omar Adrian <oadrian38@gmail.com>
2024-04-16 12:48:51 -0600

    fix two issues
    Ahora funciona el envio de datos por body-parser y ahora se eliminan los archivos despues de haber sido generados

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
 
 import { PORT } from './src/config'
 
@@ -8,9 +9,9 @@ const app = express()
 import files from './src/routes/files.routes'
 import root from './src/routes/main.routes'
 
+app.use(bodyParser.json({limit: '500kb'}))
 
 app.use('/',root) //Rutas de root
-
 app.use('/files',files) //Rutas donde se armar los archivos
 
 app.listen(PORT, ()=>{
