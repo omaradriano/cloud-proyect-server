dee51ebd2dd0ee760c59c51ed994e265453c4702
Omar Adrian <oadrian38@gmail.com>
2024-05-14 13:35:40 -0600

    intento 98484 de configurar cors

--- index.ts
+++ index.ts
@@ -5,7 +5,18 @@ import cors from 'cors'
 import { PORT } from './src/config'
 
 const app = express()
-app.use(cors({ origin: true, credentials: true }))
+
+app.use(cors({
+    origin: [
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://bisondocx.tech"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 //Routes
 import files from './src/routes/files.routes'
@@ -18,33 +29,16 @@ const whiteList: WhiteList = ["http://127.0.0.1:3000", "http://localhost:3000", 
 
 app.use(bodyParser.json({ limit: '500kb' }))
 
-const allowedOrigins = ["http://127.0.0.1:3000", "http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "https://bisondocx.tech"];
-
-const corsOptions = {
-    origin: function (origin: any, callback: any) {
-        // Verificar si el origen está en la lista de orígenes permitidos
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true); // Permitir el acceso desde el origen
-        } else {
-            callback(new Error('Not allowed by CORS')); // No permitir el acceso desde el origen
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-};
-
-app.use(cors(corsOptions)); // Habilitar CORS con las opciones especificadasc
-
 // Uso de los headers para permitir peticiones de los sitios especificados (Esto es un middleware)
-app.use((req: Request, res: Response, next: NextFunction) => {
-    const origin: string | undefined = req.headers.origin;
-    if (origin && whiteList.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     const origin: string | undefined = req.headers.origin;
+//     if (origin && whiteList.includes(origin)) {
+//         res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+//     res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+//     res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 
 app.use('/', root) //Rutas de root
